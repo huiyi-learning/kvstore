956855541b343ecf94ce5b415c9247fe5c4aa60b
wbear2 <946010351@qq.com>
2014-04-12 22:43:27 +0800

    add arguments in compress interface

--- src/main/java/cn/edu/sjtu/se/kvstore/common/CompressInterface.java
+++ src/main/java/cn/edu/sjtu/se/kvstore/common/CompressInterface.java
@@ -1,17 +1,29 @@
 package cn.edu.sjtu.se.kvstore.common;
 
-public class CompressInterface {
+import java.util.Map;
+import java.util.Set;
+
+public class CompressInterface<K,V> {
 	
 	//delete cold data 
-	public static boolean delete() {
+	//根据传入的coldKeys，将对应的压缩cold data的values删除
+	public boolean delete(Set<K> coldKeys) {
 		return true;
 	}
 	
-	//transfer hot data to cold
-	public static boolean convert() {
+	//transfer hot data to cold, namely compress data
+	//hotDatas表示已经变成cold data的hot data,将对应的values进行压缩
+	public boolean convertHotToCold(Map<K,V> hotDatas) {
 		return true;
 	}
 	
+	//transfer hot data to cold, namely decompress data
+	//coldKeys表示已经变成hot data的cold data的key，将对应的values解压出来，返回对应的K/V
+	public Map<K,V> convertColdToHot(Set<K> coldKeys) {
+		return null;
+	}
+	
+	//这个函数没有必要
 	//get cold data
 	public static String get() {
 		return "";
